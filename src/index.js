a6f401462a76ada61aa61cd0294b18a3dec82f87
aliya97k <aliyakussainova13@gmail.com>
2021-08-21 13:34:56 +0600

    feat: reverse number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
-}
+    const num = Math.abs(n);
+    return num.toString().split("").reverse().join("");
+}     
+
+    /*n = n + "";
+    if (n >= 0) {
+        return n.split("").reverse().join("");
+    } else if (n < 0) {
+        return -Math.abs(n)
+    }
+    */
